4976f14b9994df2641d322a849b53a36aaff7f41
Abhij95 <136879286+Abhij95@users.noreply.github.com>
2023-06-17 20:24:41 +0530

    Add files via upload

---
+++ large.py
@@ -0,0 +1,11 @@
+num1=int(input("enter the  number one" ))
+num2=int(input("enter the  number two "))
+num3=int(input("enter teh number three "))
+
+
+if(num1 >  num2 and num1 >= num3):
+  print( "first is largest ")
+elif(num2>num3 and num2>num1):
+  print("second is largest ")
+elif (num3>num2 and num3>num1):
+  print("third is largest")
