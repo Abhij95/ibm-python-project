62166ab2593450f16f9a2bcd6e6dbb91144c9615
Abhij95 <136879286+Abhij95@users.noreply.github.com>
2023-06-17 21:51:26 +0530

    Add files via upload

---
+++ marks.py
@@ -0,0 +1,12 @@
+marks=int(input("enter the marks"))
+if (marks>=90 & marks<=100):
+    print("A+")
+elif(marks>=80 & marks<=90):
+ print("A")
+elif(marks>=70 & marks<=80):
+ print("B")
+elif(marks>=60 & marks<=70):
+ print("C")
+elif(marks>=50 & marks<=60):
+  print("D")
+                    
